60b713cc9f03bf2c198a0e679413b5b24b750f07
Henderson da Rocha Porfirio <hendersonporfirio@protonmail.com>
2022-10-14 17:19:06 -0300

    #Commit - modificando as properties

--- src/main/java/com/tuyo/loans/LoansConfigServerApplication.java
+++ src/main/java/com/tuyo/loans/LoansConfigServerApplication.java
@@ -3,10 +3,12 @@ package com.tuyo.loans;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.autoconfigure.domain.*;
+import org.springframework.cloud.context.config.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({ @ComponentScan("com.tuyo.loans.controller") })
 @EnableJpaRepositories("com.tuyo.loans.repository")
 @EntityScan("com.tuyo.loans.model")
